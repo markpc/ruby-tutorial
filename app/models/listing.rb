8f12bd9fb3215ca75758538be4054e57dbab5f3f
Mark Powell Christiansen <powellchrist@gmail.com>
2014-04-27 20:08:50 +0200

    changed listing.rb back to standard

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,6 @@
-class Listing < ActiveRecord::Base  
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-    :storage => :dropbox,
-   :dropbox_credentials => Rails.root.join("config/dropbox.yml")
- validates_attachment_content_type :image, :content_type => %%w(image/jpeg image/jpg image/png)
+class Listing < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "250x", :thumb => "100x100" }, :default_url => "default.jpg"
+  validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ },
+  :storage => :dropbox,
+  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 end
-
