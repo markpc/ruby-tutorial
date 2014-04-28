17a7f696703fa814e16b7166cfb427715e7f0c74
Mark Powell Christiansen <powellchrist@gmail.com>
2014-04-28 10:47:40 +0200

    deleted pictures on local host

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "250x", :thumb => "100x100" }, :default_url => "default.jpg"
-  					:storage => :dropbox
+  has_attached_file :image, :styles => { :medium => "250x", :thumb => "100x100" }, 
+  					:default_url => "default.jpg",
+  					:storage => :dropbox,
   					:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  					validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+ 					validates_attachment :image, :content_type => { content_type: /\Aimage\/.*\Z/ }
 end
