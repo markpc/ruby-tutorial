db22dcd78960a44339049fc78cf3c2c293e99d25
Mark Powell Christiansen <powellchrist@gmail.com>
2014-04-28 07:52:17 +0200

    more indenting

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "250x", :thumb => "100x100" }, :default_url => "default.jpg"
-  					:storage => :dropbox,
-  					:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-  validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+  					:storage => :dropbox
+  					:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  					validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
 end
